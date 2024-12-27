40e8f8106dfe4077c2f0800994e674a3a6cb3507
Jang <sdsd090811@gmail.com>
2024-12-27 16:44:24 +0900

    feat: 타임리프로 데이터 렌더링 기능 추가

--- src/main/java/com/bookspot/library/StockController.java
+++ src/main/java/com/bookspot/library/StockController.java
@@ -2,7 +2,10 @@ package com.bookspot.library;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,7 +16,16 @@ public class StockController {
     }
 
     @GetMapping("/libraries/stock")
-    public String searchLibrariesStock(StockSearchForm stockSearchForm) {
+    public String searchLibrariesStock(Model model, StockSearchForm stockSearchForm) {
+        model.addAttribute("contents", List.of(
+                new LibraryStockDto("A도서관", 3.41, 10, 6, List.of(
+                        "AAA", "BBB", "CCC", "DDD"
+                )),
+                new LibraryStockDto("B도서관", 4.41, 10, 8, List.of(
+                        "BBB", "CCC"
+                ))
+        ));
+
         return "libraries/stock/search";
     }
 
