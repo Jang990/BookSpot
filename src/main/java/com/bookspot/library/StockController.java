91b1ac17bb057c39d25d9218e97407bba8cf073c
Jang <sdsd090811@gmail.com>
2024-12-28 14:01:15 +0900

    feat: 검색 폼 검증 코드 추가

--- src/main/java/com/bookspot/library/StockController.java
+++ src/main/java/com/bookspot/library/StockController.java
@@ -1,8 +1,10 @@
 package com.bookspot.library;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -11,12 +13,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class StockController {
     @GetMapping("/libraries/stock/search-setting")
-    public String settingPage() {
+    public String settingPage(Model model) {
+        model.addAttribute("stockSearchForm", new StockSearchForm());
         return "libraries/stock/search-setting";
     }
 
     @GetMapping("/libraries/stock")
-    public String searchLibrariesStock(Model model, StockSearchForm stockSearchForm) {
+    public String searchLibrariesStock(
+            Model model,
+            @Valid StockSearchForm stockSearchForm,
+            BindingResult bindingResult) {
+        if(bindingResult.hasErrors())
+            return "libraries/stock/search-setting";
+
         model.addAttribute("contents", List.of(
                 new LibraryStockDto("A도서관", 3.41, 10, 6, List.of(
                         "AAA", "BBB", "CCC", "DDD"
--- src/main/java/com/bookspot/library/StockSearchForm.java
+++ src/main/java/com/bookspot/library/StockSearchForm.java
@@ -1,13 +1,22 @@
 package com.bookspot.library;
 
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 import java.util.List;
 
 @Data
 public class StockSearchForm {
-    private double latitude;
-    private double longitude;
+    @NotNull
+    @Range(min = -90, max = 90)
+    private Double latitude;
+
+    @NotNull
+    @Range(min = -180, max = 180)
+    private Double longitude;
+
+    @Size(max = 10)
     private List<Long> bookId;
 }
