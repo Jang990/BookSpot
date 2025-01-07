a61cecfc628abadaf992c76c8988cac370c22e43
Jang_Laptop <sdsd090811@gmail.com>
2025-01-07 15:43:10 +0900

    feat: 책 검색 컨트롤러 추가

--- src/main/java/com/bookspot/book/controller/BookController.java
+++ src/main/java/com/bookspot/book/controller/BookController.java
@@ -4,18 +4,25 @@ import com.bookspot.book.application.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
 
     @GetMapping("/api/book")
+    @ResponseBody
     public Slice<BookSummaryResponse> findBook(
             String title,
             Pageable pageable) {
         return bookService.findBook(title, pageable);
+    }
+
+    @GetMapping("/libraries/stock/book")
+    public String searchBookPage() {
+        return "book/search";
     }
 }
