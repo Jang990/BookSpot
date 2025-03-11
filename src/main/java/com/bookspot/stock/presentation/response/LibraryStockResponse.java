21405637a3cdb4e2fece4a892013f2f1e9031603
Jang <sdsd090811@gmail.com>
2025-03-11 21:26:33 +0900

    refactor: 도서관 재고의 totalCount getter를 필드로 변경

--- src/main/java/com/bookspot/stock/presentation/response/LibraryStockResponse.java
+++ src/main/java/com/bookspot/stock/presentation/response/LibraryStockResponse.java
@@ -2,8 +2,8 @@ package com.bookspot.stock.presentation.response;
 
 import java.util.List;
 
-public record LibraryStockResponse(Long libraryId, List<Long> availableBookIds, List<Long> unavailableBookIds) {
-    public int totalBooksCount() {
-        return availableBookIds.size() + unavailableBookIds.size();
+public record LibraryStockResponse(Long libraryId, int totalBooksCount,List<Long> availableBookIds, List<Long> unavailableBookIds) {
+    public LibraryStockResponse(Long libraryId, List<Long> availableBookIds, List<Long> unavailableBookIds) {
+        this(libraryId, availableBookIds.size() + unavailableBookIds.size(), availableBookIds, unavailableBookIds);
     }
 }
