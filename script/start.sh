dfe8a3e71f6899a901e672dfe7c2e404423217b1
Jang <sdsd090811@gmail.com>
2025-06-21 15:59:43 +0900

    feat: 배포 시 로그 관리 스크립트 추가

--- script/start.sh
+++ script/start.sh
@@ -27,8 +27,26 @@ echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
+# 이전 로그 보관
+CURRENT_TIME=$(date +%Y%m%d-%H%M%S)
+LOG_FILENAME=deployed-at-${CURRENT_TIME}-${PROJECT_NAME}-app.log
+LOG_FILE=$REPOSITORY/${LOG_FILENAME}
+LOG_DIR=$REPOSITORY/logs
+PREVIOUS_LOGS=$(find $REPOSITORY -maxdepth 1 -name "*-${PROJECT_NAME}-app.log")
+
+if [ -n "$PREVIOUS_LOGS" ]; then
+  echo "> 기존 로그 파일을 $LOG_DIR/ 로 이동합니다:"
+  for file in $PREVIOUS_LOGS; do
+    filename=$(basename $file)
+    echo "  - $filename"
+    mv "$file" "$LOG_DIR/"
+  done
+else
+  echo "> 이전 로그 파일이 없습니다."
+fi
+
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
     -Dspring.config.location=classpath:/application-$IDLE_PROFILE.yml,/home/ec2-user/app/project/application-prod.yml \
     -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    $JAR_NAME > ${LOG_FILE} 2>&1 &
