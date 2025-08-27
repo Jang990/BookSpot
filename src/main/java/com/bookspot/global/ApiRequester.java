d1e2b6807021e36917edc08f36ce10e2fd570dfc
Jang <sdsd090811@gmail.com>
2025-08-27 11:04:31 +0900

    feat: 외부 API에 요청을 보내는 ApiRequester 추가

---
+++ src/main/java/com/bookspot/global/ApiRequester.java
@@ -0,0 +1,26 @@
+package com.bookspot.global;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+@Component
+public class ApiRequester {
+    private final WebClient client;
+
+    public ApiRequester() {
+        client = WebClient.builder()
+                .defaultHeader("Content-type", "application/x-www-form-urlencoded;charset=utf-8")
+                .build();
+    }
+
+    public <T> T get(String url, Class<T> responseType) {
+        log.trace("{} API 요청 시도", url);
+        return client.get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(responseType)
+                .block();
+    }
+}
