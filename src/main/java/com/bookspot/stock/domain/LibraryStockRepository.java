5cc8455308f3a1c66316a7bd2a7ae591f134e8ee
Jang <sdsd090811@gmail.com>
2025-08-28 16:52:45 +0900

    feat: findStock 메소드에 비관적 락 추가

--- src/main/java/com/bookspot/stock/domain/LibraryStockRepository.java
+++ src/main/java/com/bookspot/stock/domain/LibraryStockRepository.java
@@ -1,7 +1,11 @@
 package com.bookspot.stock.domain;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -18,5 +22,7 @@ public interface LibraryStockRepository extends JpaRepository<LibraryStock, Long
                 join fetch ls.library l
             where ls.id = :id
             """)
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "10_000")})
     Optional<LibraryStock> findStock(@Param("id") Long id);
 }
