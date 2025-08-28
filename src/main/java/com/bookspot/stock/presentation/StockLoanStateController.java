bba610a8aea7dcf04b99e73fec9662c5d994cbbd
Jang <sdsd090811@gmail.com>
2025-08-28 17:20:45 +0900

    feat: /api/stocks/{stockId}/loan/refresh api 추가

---
+++ src/main/java/com/bookspot/stock/presentation/StockLoanStateController.java
@@ -0,0 +1,24 @@
+package com.bookspot.stock.presentation;
+
+import com.bookspot.global.log.BasicLog;
+import com.bookspot.stock.application.LibraryStockRefreshService;
+import com.bookspot.stock.presentation.response.StockLoanStateResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@BasicLog
+@RestController
+@RequiredArgsConstructor
+public class StockLoanStateController {
+    private final LibraryStockRefreshService refreshService;
+
+    @PostMapping("/api/stocks/{stockId}/loan/refresh")
+    public ResponseEntity<StockLoanStateResponse> refresh(
+            @PathVariable("stockId") long stockId
+    ) {
+        return ResponseEntity.ok(refreshService.findCurrentLoanState(stockId));
+    }
+}
