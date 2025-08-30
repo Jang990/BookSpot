b39d46019400b888561ee376d57e68a58c59ec64
Jang <sdsd090811@gmail.com>
2025-08-30 10:35:52 +0900

    fix: jpa auditing 리스너 생략으로 인한 날짜 업데이트 누락 문제 해결

--- src/main/java/com/bookspot/stock/domain/LibraryStock.java
+++ src/main/java/com/bookspot/stock/domain/LibraryStock.java
@@ -9,12 +9,14 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class LibraryStock {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
