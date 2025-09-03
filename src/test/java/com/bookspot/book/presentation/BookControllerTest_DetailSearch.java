54eaf12016af250300d680c6643c1225bb8b8391
Jang <sdsd090811@gmail.com>
2025-09-03 16:29:45 +0900

    test: 컨트롤러 테스트 시에 시큐리티 설정을 따르도록 변경

--- src/test/java/com/bookspot/book/presentation/BookControllerTest_DetailSearch.java
+++ src/test/java/com/bookspot/book/presentation/BookControllerTest_DetailSearch.java
@@ -1,11 +1,13 @@
 package com.bookspot.book.presentation;
 
 import com.bookspot.book.application.BookService;
+import com.bookspot.global.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("책 상세정보 검색 테스트")
 @WebMvcTest(BookController.class)
 @MockBean(JpaMetamodelMappingContext.class)
+@Import(SecurityConfig.class)
 class BookControllerTest_DetailSearch {
 
     @Autowired MockMvc mvc;
