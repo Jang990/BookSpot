c5eeb518277856a7ab66b77807fbf6f84f60d928
Jang <sdsd090811@gmail.com>
2025-09-05 16:08:07 +0900

    feat: String을  UsersRole로 바꿔주는 코드 추가

--- src/main/java/com/bookspot/users/domain/UsersRole.java
+++ src/main/java/com/bookspot/users/domain/UsersRole.java
@@ -1,5 +1,16 @@
 package com.bookspot.users.domain;
 
 public enum UsersRole {
-    USER, ADMIN
+    USER, ADMIN;
+
+    public static UsersRole fromString(String role) {
+        if (role == null)
+            throw new IllegalArgumentException("Role 변경 시 null 불가능");
+
+        try {
+            return UsersRole.valueOf(role.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("잘못된 Role String : " + role);
+        }
+    }
 }
