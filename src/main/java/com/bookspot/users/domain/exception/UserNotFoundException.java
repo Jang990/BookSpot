107414faaba4d79b4876e5f815c3e81bbd3c3401
Jang <sdsd090811@gmail.com>
2025-09-10 20:10:36 +0900

    feat: UserNotFoundException 추가

---
+++ src/main/java/com/bookspot/users/domain/exception/UserNotFoundException.java
@@ -0,0 +1,16 @@
+package com.bookspot.users.domain.exception;
+
+import com.bookspot.global.error.AppException;
+import org.springframework.http.HttpStatus;
+
+public class UserNotFoundException extends AppException {
+
+    public UserNotFoundException(long userId) {
+        super(
+            "USER_NOT_FOUND",
+            "사용자(%d)를 DB에서 찾을 수 없습니다.".formatted(userId),
+            "사용자를 찾을 수 없습니다.",
+            HttpStatus.NOT_FOUND
+        );
+    }
+}
