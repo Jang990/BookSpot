74e8a18a552c0a920c77f5c5e0dc5eda306051d8
Jang <sdsd090811@gmail.com>
2025-09-13 16:12:12 +0900

    refacor: 테스트 용이성을 위해서 토큰 검증기 헬퍼 클래스를 컴포넌트로 변경

--- src/main/java/com/bookspot/users/application/helper/SocialTokenVerifierSelector.java
+++ src/main/java/com/bookspot/users/application/helper/SocialTokenVerifierSelector.java
@@ -2,12 +2,18 @@ package com.bookspot.users.application.helper;
 
 import com.bookspot.users.domain.OAuthProvider;
 import com.bookspot.users.domain.SocialTokenVerifier;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
+@RequiredArgsConstructor
 public class SocialTokenVerifierSelector {
-    public static SocialTokenVerifier select(List<SocialTokenVerifier> socialTokenVerifiers, OAuthProvider oAuthProvider) {
-        return socialTokenVerifiers.stream()
+    private final List<SocialTokenVerifier> socialTokenVerifierList;
+
+    public SocialTokenVerifier select(OAuthProvider oAuthProvider) {
+        return socialTokenVerifierList.stream()
                 .filter(socialTokenVerifier -> socialTokenVerifier.supports(oAuthProvider))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 OAuth"));
