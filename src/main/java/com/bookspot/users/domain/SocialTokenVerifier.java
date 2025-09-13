1e531a43ac384e3da8e53df3ec03b5ec997dc3d0
Jang <sdsd090811@gmail.com>
2025-09-13 15:52:41 +0900

    feat: SocialTokenVerifier에 provider 지원여부를 묻는 메소드 추가

--- src/main/java/com/bookspot/users/domain/SocialTokenVerifier.java
+++ src/main/java/com/bookspot/users/domain/SocialTokenVerifier.java
@@ -2,4 +2,5 @@ package com.bookspot.users.domain;
 
 public interface SocialTokenVerifier {
     SocialTokenDetail verifyToken(String idTokenString);
+    boolean supports(OAuthProvider provider);
 }
