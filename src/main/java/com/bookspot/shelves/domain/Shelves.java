b127f0c66313b7ba82115995d3644925b19b534c
Jang <sdsd090811@gmail.com>
2025-09-30 13:42:52 +0900

    feat: 책장 엔티티 추가

---
+++ src/main/java/com/bookspot/shelves/domain/Shelves.java
@@ -0,0 +1,40 @@
+package com.bookspot.shelves.domain;
+
+import com.bookspot.users.domain.Users;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Shelves {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int bookCount;
+    private boolean isPublic;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users users;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public Shelves(Users users, String name, boolean isPublic) {
+        this.users = users;
+        this.name = name;
+        this.isPublic = isPublic;
+        this.bookCount = 0;
+    }
+}
