6721aae764e9c4b3f11a52b8ace351d7d1251ae3
Jang <sdsd090811@gmail.com>
2025-09-30 14:19:39 +0900

    featt: 책장 생성 시 로그인 권한 요구 로직 추가

--- src/main/java/com/bookspot/shelves/presentation/ShelvesManageController.java
+++ src/main/java/com/bookspot/shelves/presentation/ShelvesManageController.java
@@ -2,17 +2,22 @@ package com.bookspot.shelves.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@PreAuthorize("isAuthenticated()")
 @RequestMapping("/api/users/shelves/{shelfId}")
 @RestController
 @RequiredArgsConstructor
 public class ShelvesManageController {
+
     @PostMapping
     public ResponseEntity<Void> addShelf(
+            @AuthenticationPrincipal String userIdStr,
             @PathVariable("shelfId") long shelfId
     ) {
         return ResponseEntity.ok().build();
--- src/test/java/com/bookspot/shelves/presentation/ShelvesManageControllerTest.java
+++ src/test/java/com/bookspot/shelves/presentation/ShelvesManageControllerTest.java
@@ -1,29 +1,55 @@
 package com.bookspot.shelves.presentation;
 
-import com.bookspot.ControllerTestNoSecurity;
+import com.bookspot.global.auth.JwtProvider;
+import com.bookspot.global.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.util.List;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ShelvesManageController.class)
-@ControllerTestNoSecurity
+@Import(SecurityConfig.class)
+@MockBean(JpaMetamodelMappingContext.class)
 class ShelvesManageControllerTest {
+    private static final String COMMON_PATH = "/api/users/shelves/1";
+
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private JwtProvider jwtProvider;
+
     @Test
-    void 테스트() throws Exception {
-        mockMvc.perform(post("/api/users/shelves/1"))
-                .andExpect(status().isOk());
+    void 로그인하지_않은_사용자의_생성은_불가능() throws Exception {
+        mockMvc.perform(post(COMMON_PATH))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void 로그인한_사용자의_생성() throws Exception {
+        mockMvc.perform(
+                        post(COMMON_PATH)
+                        .with(authentication(userAuth(10L)))
+                )
+                .andExpect(status().isUnauthorized());
+    }
+
+    private static UsernamePasswordAuthenticationToken userAuth(long userId) {
+        return new UsernamePasswordAuthenticationToken(
+                Long.toString(userId), null,
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
     }
 
 }
