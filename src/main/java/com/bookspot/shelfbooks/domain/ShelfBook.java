f400bcbe133bbe56f10d8209c9760fc074a1482a
Jang <sdsd090811@gmail.com>
2025-10-02 18:15:06 +0900

    feat: ShelfBook에 createdAt updatedAt 필드 추가

--- src/main/java/com/bookspot/shelfbooks/domain/ShelfBook.java
+++ src/main/java/com/bookspot/shelfbooks/domain/ShelfBook.java
@@ -6,11 +6,17 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "shelf_books")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class ShelfBook {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,6 +31,11 @@ public class ShelfBook {
     private Book book;
 
     private int idx;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
     public ShelfBook(Shelves shelf, Book book, int idx) {
         this.shelf = shelf;
