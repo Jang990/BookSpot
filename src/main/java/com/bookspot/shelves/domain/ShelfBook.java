7e100a6cb079572c0fd562c27ee0e4da772e4548
Jang <sdsd090811@gmail.com>
2025-10-02 16:18:50 +0900

    feat: ShelfBook 엔티티 추가

---
+++ src/main/java/com/bookspot/shelves/domain/ShelfBook.java
@@ -0,0 +1,33 @@
+package com.bookspot.shelves.domain;
+
+import com.bookspot.book.domain.Book;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "shelf_books")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ShelfBook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shelf_id", nullable = false)
+    private Shelves shelf;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    private int idx;
+
+    protected ShelfBook(Shelves shelf, Book book, int idx) {
+        this.shelf = shelf;
+        this.book = book;
+        this.idx = idx;
+    }
+}
