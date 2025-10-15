f820ac8d162c83e1ea9558c581f558aed332f5e2
Jang_Laptop <sdsd090811@gmail.com>
2025-10-15 16:15:50 +0900

    feat: OpenSearchPageable에서 pageNumber까지 다루도록 변경

--- src/main/java/com/bookspot/book/infra/search/pagination/OpenSearchPageable.java
+++ src/main/java/com/bookspot/book/infra/search/pagination/OpenSearchPageable.java
@@ -1,23 +1,19 @@
 package com.bookspot.book.infra.search.pagination;
 
-import lombok.Getter;
 import org.opensearch.client.opensearch._types.SortOptions;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-@Getter
 public class OpenSearchPageable {
-    private final int offset;
-    private final int pageSize;
+    private final Pageable pageable;
     private final List<SortOptions> sortOptions;
 
     private OpenSearchPageable(Pageable pageable, List<SortOptions> sortOptions) {
         if(pageable.getOffset() + pageable.getPageSize() >= 10_000)
             throw new IllegalArgumentException("Pageable 검색 시 1만건 이하의 offset만 검색 가능");
 
-        this.offset = Math.toIntExact(pageable.getOffset());
-        this.pageSize = pageable.getPageSize();
+        this.pageable = pageable;
         this.sortOptions = sortOptions;
     }
 
@@ -42,6 +38,22 @@ public class OpenSearchPageable {
 
     public static OpenSearchPageable withRank(Pageable pageable) {
         return new OpenSearchPageable(pageable, BookSortOptions.RANKING_SORT);
+    }
+
+    public int getPageSize() {
+        return pageable.getPageSize();
+    }
+
+    public int getPageNumber() {
+        return pageable.getPageNumber();
+    }
+
+    public int getOffset() {
+        return (int) pageable.getOffset();
+    }
+
+    public List<SortOptions> getSortOptions() {
+        return sortOptions;
     }
 
     public boolean hasScoreSortOption() {
