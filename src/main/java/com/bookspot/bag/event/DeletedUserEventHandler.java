ada6fc0fc2b84573b050c9bb3efdfaff3334f0d3
Jang <sdsd090811@gmail.com>
2025-11-08 11:49:48 +0900

    feat: 회원 탈퇴 시 책가방 비우기

---
+++ src/main/java/com/bookspot/bag/event/DeletedUserEventHandler.java
@@ -0,0 +1,21 @@
+package com.bookspot.bag.event;
+
+import com.bookspot.bag.domain.BagBookRepository;
+import com.bookspot.users.domain.event.BookAddedToBagEvent;
+import com.bookspot.users.domain.event.DeletedUserEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class DeletedUserEventHandler {
+    private final BagBookRepository bagBookRepository;
+
+    @EventListener(DeletedUserEvent.class)
+    public void handle(DeletedUserEvent event) {
+        bagBookRepository.deleteByUsersId(event.userId());
+    }
+}
