5cada543038d49f6b26b76597c1acb227f4d24a2
Jang <sdsd090811@gmail.com>
2025-11-09 12:02:47 +0900

    feat: api에 userAuth를 추가해주는 test 전용 헬퍼 메소드

--- src/test/java/com/bookspot/WebSecurityAuthHelper.java
+++ src/test/java/com/bookspot/WebSecurityAuthHelper.java
@@ -2,12 +2,18 @@ package com.bookspot;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import java.util.List;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 
 public class WebSecurityAuthHelper {
+
+    public static MockHttpServletRequestBuilder apiWithAuth(MockHttpServletRequestBuilder request, long userId) {
+        return request.with(authentication(userAuth(userId)));
+    }
+
     public static UsernamePasswordAuthenticationToken userAuth(long userId) {
         return new UsernamePasswordAuthenticationToken(
                 Long.toString(userId), null,
